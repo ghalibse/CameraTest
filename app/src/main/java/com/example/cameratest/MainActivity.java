67392ae6e2587e9cca23f88ef3a7479f3034f777
ghalibse <ghalibse@gmail.com>
2016-07-29 13:14:31 -0400

    First Commit

---
+++ app/src/main/java/com/example/cameratest/MainActivity.java
@@ -0,0 +1,30 @@
+package com.example.cameratest;
+
+import android.content.Intent;
+import android.provider.MediaStore;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+
+    Button btn;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        btn = (Button) findViewById(R.id.btn_pic);
+
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+                startActivityForResult(intent, 0);
+            }
+        });
+
+
+    }
+}
